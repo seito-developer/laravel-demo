5d21abf359c35ef307a020bc239065ea90961d28
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-11-20 00:18:18 +0900

    feat: update models

--- demo-app/app/Models/Project.php
+++ demo-app/app/Models/Project.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
-    //
+    use HasFactory;
+    
+    // (CH3で使います)
+    protected $fillable = ['name', 'user_id']; 
+
+    /**
+     * このプロジェクトを所有するユーザーを取得
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    /**
+     * このプロジェクトに属するタスクを取得
+     */
+    public function todos(): HasMany
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
