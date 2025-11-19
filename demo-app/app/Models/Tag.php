8e1ed3275e3b30bd42d84dc77d21f20fc80539b3
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-11-20 00:19:02 +0900

    feat: update models 2

--- demo-app/app/Models/Tag.php
+++ demo-app/app/Models/Tag.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
-    //
+    use HasFactory;
+
+    // (CH3で使います)
+    protected $fillable = ['name'];
+
+    /**
+     * このタグが紐付くタスクを取得
+     */
+    public function todos(): BelongsToMany
+    {
+        return $this->belongsToMany(Todo::class);
+    }
 }
