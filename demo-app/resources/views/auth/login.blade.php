9acf72a27fdb6cad417140ef7a377ed17ee8e60c
Seito Horiguchi <seito.horiguchi@gmail.com>
2025-11-20 01:29:15 +0900

    feat: update view and route

--- demo-app/resources/views/auth/login.blade.php
+++ demo-app/resources/views/auth/login.blade.php
@@ -4,27 +4,36 @@
 
 @section('content')
 
+    <h2>ログイン</h2>
+
     @if ($errors->any())
-    <div>
-        <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-        </ul>
-    </div>
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     @endif
-    <p>ログイン画面</p>
+
     <form method="POST" action="{{ route('login') }}">
-    @csrf
-    <div>
-        <label for="email">メールアドレス</label>
-        <input id="email" type="email" name="email">
-    </div>
-    <div>
-        <label for="password">パスワード</label>
-        <input id="password" type="password" name="password">
-    </div>
-    <button type="submit">ログイン</button>
-    <a href="{{ route('register') }}">ユーザー登録</a>
+        @csrf <div>
+            <label for="email">メールアドレス</label>
+            <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+        </div>
+        <div>
+            <label for="password">パスワード</label>
+            <input id="password" type="password" name="password" required>
+        </div>
+
+        <div>
+            <label>
+                <input type="checkbox" name="remember"> ログイン状態を記憶する
+            </label>
+        </div>
+
+        <div>
+            <button type="submit">ログイン</button>
+        </div>
     </form>
 @endsection
--- demo-app/routes/web.php
+++ demo-app/routes/web.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/home', 'auth.home')->middleware('auth'); // ホーム画面の表示処理
+// Route::view('/home', 'auth.home')->middleware('auth'); // ホーム画面の表示処理
 
 Route::middleware('auth')->group(function () {
+
+    // Route::view('/home', 'auth.home');
+    Route::get('/home', function () {
+        return view('auth.home');
+    });
 
     // タスクのCRUD (一覧、作成、保存、詳細、編集、更新、削除)
     Route::resource('projects', ProjectController::class);
